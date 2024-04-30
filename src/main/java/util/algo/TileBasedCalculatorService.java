2e9f477cc25cd7ade740e1e2c54a549f05db43e0
pvassil <panos.vassiliadis@gmail.com>
2024-04-30 14:32:55 +0300

    ready to merge into main

--- src/main/java/util/algo/TileBasedCalculatorService.java
+++ src/main/java/util/algo/TileBasedCalculatorService.java
@@ -7,45 +7,45 @@ import util.tiles.ITile;
 public class TileBasedCalculatorService {
 	protected static final boolean DEBUG_FLAG = false;
 
-    private ITilesManager tilesManager;
+	private ITilesManager tilesManager;
 
 
-    public TileBasedCalculatorService(ITilesManager tilesManager) {
-        this.tilesManager = tilesManager;
-    }
+	public TileBasedCalculatorService(ITilesManager tilesManager) {
+		this.tilesManager = tilesManager;
+	}
 
-    public double calculateKendallTauCorrelation(AlgoEnum algorithm) throws IllegalArgumentException {
-        CorrelationStatistics statistics = new CorrelationStatistics();
-        CalculationTimer timer = new CalculationTimer();
+	public double calculateKendallTauCorrelation(AlgoEnum algorithm) throws IllegalArgumentException {
+		CorrelationStatistics statistics = new CorrelationStatistics();
+		CalculationTimer timer = new CalculationTimer();
 
-        ITile[][] tiles = tilesManager.createTilesArray();
-        
-        //TODO FIX, REFACTOR, Produce iface
-        switch(algorithm) {
-        case SIMPLE_TILES_AND_POINT_COMPARISONS:
-        	AlgoSimpleTilesAndPointComparison processorSimple = new AlgoSimpleTilesAndPointComparison(tiles, statistics);
-        	processorSimple.processAllTiles();
-        	break;
-//        case BANDS_WITH_VISIT_MEMORY:
-//            AlgoBandsWithVisitMemory processorWithMemory = new AlgoBandsWithVisitMemory(tiles, statistics);
-//            processorWithMemory.processAllTiles();
-//            break;
-        case SPARK_TILES_ALGO:
-        	AlgoSimpleTilesAndPointComparison processorSimpleSpark = new AlgoSimpleTilesAndPointComparison(tiles, statistics);
-        	processorSimpleSpark.processAllTiles();
-        	break;
-        default:
-            throw new IllegalArgumentException(
-                    String.format("%s is not a supported calculation method.", algorithm));
-//			break;
-        }
+		ITile[][] tiles = tilesManager.createTilesArray();
 
-        if(DEBUG_FLAG) {
-        	System.out.println(statistics);
-        	System.out.println(timer);
-        }
-        return statistics.calculateCorrelationResult();
-    }
+		//TODO FIX, REFACTOR, Produce iface
+		switch(algorithm) {
+		case SIMPLE_TILES_AND_POINT_COMPARISONS:
+			AlgoSimpleTilesAndPointComparison processorSimple = new AlgoSimpleTilesAndPointComparison(tiles, statistics);
+			processorSimple.processAllTiles();
+			break;
+			//        case BANDS_WITH_VISIT_MEMORY:
+			//            AlgoBandsWithVisitMemory processorWithMemory = new AlgoBandsWithVisitMemory(tiles, statistics);
+			//            processorWithMemory.processAllTiles();
+			//            break;
+		case SPARK_TILES_ALGO:
+			AlgoSimpleTilesAndPointComparison processorSimpleSpark = new AlgoSimpleTilesAndPointComparison(tiles, statistics);
+			processorSimpleSpark.processAllTiles();
+			break;
+		default:
+			throw new IllegalArgumentException(
+					String.format("%s is not a supported calculation method.", algorithm));
+			//			break;
+		}
+
+		if(DEBUG_FLAG) {
+			System.out.println(statistics);
+			System.out.println(timer);
+		}
+		return statistics.calculateCorrelationResult();
+	}
 }
 
 
--- src/main/java/util/algo/TileXBasedCalculatorService.java
+++ src/main/java/util/algo/TileXBasedCalculatorService.java
@@ -6,27 +6,27 @@ import util.tiles.TileWithCounters;
 public class TileXBasedCalculatorService {
 	protected static final boolean DEBUG_FLAG = false;
 
-    private TilesManagerListBasedTilesWithCounters tilesManager;
+	private TilesManagerListBasedTilesWithCounters tilesManager;
 
 
-    public TileXBasedCalculatorService(TilesManagerListBasedTilesWithCounters tilesManager) {
-        this.tilesManager = tilesManager;
-    }
+	public TileXBasedCalculatorService(TilesManagerListBasedTilesWithCounters tilesManager) {
+		this.tilesManager = tilesManager;
+	}
 
-    public double calculateKendallTauCorrelation() {
-        CorrelationStatistics statistics = new CorrelationStatistics();
-        CalculationTimer timer = new CalculationTimer();
+	public double calculateKendallTauCorrelation() {
+		CorrelationStatistics statistics = new CorrelationStatistics();
+		CalculationTimer timer = new CalculationTimer();
 
-        TileWithCounters[][] tiles = tilesManager.createTilesArray();
-        
-            AlgoBandsWithVisitMemory processorWithMemory = new AlgoBandsWithVisitMemory(tiles, statistics);
-            processorWithMemory.processAllTiles();
-        if(DEBUG_FLAG) {
-        	System.out.println(statistics);
-        	System.out.println(timer);
-        }
-        return statistics.calculateCorrelationResult();
-    }
+		TileWithCounters[][] tiles = tilesManager.createTilesArray();
+
+		AlgoBandsWithVisitMemory processorWithMemory = new AlgoBandsWithVisitMemory(tiles, statistics);
+		processorWithMemory.processAllTiles();
+		if(DEBUG_FLAG) {
+			System.out.println(statistics);
+			System.out.println(timer);
+		}
+		return statistics.calculateCorrelationResult();
+	}
 }
 
 
